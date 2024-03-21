e619ed966f47a3919ff88f7e2947588e0bf15550
오홍석 <lkl4502@kookmin.ac.kr>
2024-03-21 20:46:48 +0900

    feat : rgb 정보 추출해서 csv로 변환하는 코드 함수화

--- Color_extract/color.py
+++ Color_extract/color.py
@@ -1,5 +1,9 @@
+import sys
+import os
 import cv2
 import numpy as np
+import pandas as pd
+from Skin_detect.skin_detect_v2 import *
 
 def extract_color(mask, path):
     img = cv2.imread(path)
@@ -7,4 +11,45 @@ def extract_color(mask, path):
 
     points = np.argwhere(mask)
     return rgb_img[points[:, 0], points[:, 1], :]
-    
+
+def save_rgb_data(csv_path, folder_name):
+    sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+
+    df = pd.read_csv(csv_path)
+
+    # columns 명의 공백 제거
+    df.columns = [x.replace(' ', '') for x in df.columns]
+
+    # rgb 색상 저장할 list 생성
+    red = [0] * len(df)
+    green = [0] * len(df)
+    blue = [0] * len(df)
+
+    for idx, file in enumerate(df['filename']):
+        # 색상정보 검출할 파일 경로
+        path = os.path.join(os.getcwd(), folder_name, "train", file)
+        total_feat_mask = get_mask(path)
+
+        face_mask = get_feature_mask(total_feat_mask, FaceFeature.FACE)
+        nose_mask = get_feature_mask(total_feat_mask, FaceFeature.NOSE)
+
+        face_nose_mask = combine_feature(face_mask, nose_mask)
+
+        face_img = extract_feature(path, face_nose_mask)
+
+        rgb = extract_color(face_img, path)
+
+        rgb_mean = rgb.mean(axis=0).round()
+
+        red[idx] = rgb_mean[0]
+        green[idx] = rgb_mean[1]
+        blue[idx] = rgb_mean[2]
+
+        print("진행률 : {} / {}\n".format(idx, len(df)))
+
+    rgb_data = {'Red' : red, 'Green' : green, 'Blue': blue}
+    rgb_df = pd.DataFrame(rgb_data)
+
+    total_df = pd.concat([df, rgb_df], axis=1)
+
+    total_df.to_csv(path_or_buf = csv_path, mode='w')

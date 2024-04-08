b146a2c6e0dc350a9dfc6be51690b2319d8c176d
오홍석 <lkl4502@kookmin.ac.kr>
2024-04-09 00:24:17 +0900

    feat : image augmentation을 적용하여 데이터 증가

--- image_augmentation/image_augmentation.py
+++ image_augmentation/image_augmentation.py
@@ -10,9 +10,31 @@ def image_flipping(img, type):
 def image_rotation(img, angle):
     return cv2.rotate(img, angle)
 
-path = os.path.dirname(os.path.dirname(__file__)) + "/test/kimmingyu_rotation_270.jpeg"
-print(path)
-image = cv2.imread("./test/kimmingyu.jpeg")
+path = os.path.dirname(os.path.dirname(__file__)) + "/personal_color_dataset/train/"
 
-cv2.imwrite(path, image_rotation(image, cv2.ROTATE_90_COUNTERCLOCKWISE))
+df = pd.read_csv(path + "data.csv")
 
+files = df['filename']
+labels = df['label']
+
+
+namelist = ["_rotation_90.jpg", "_rotation_180.jpg", "_rotation_270.jpg", "_flip_horizontal.jpg"]
+
+idx = df.index[-1] + 1
+
+for file, label in zip(files, labels):
+    image = cv2.imread(path + file)
+    for i, name in enumerate(namelist):
+        filename = path + file[ : -4] + name
+        if i == 0:
+            cv2.imwrite(filename, image_rotation(image, cv2.ROTATE_90_CLOCKWISE))
+        elif i == 1:
+            cv2.imwrite(filename, image_rotation(image, cv2.ROTATE_180))
+        elif i == 2:
+            cv2.imwrite(filename, image_rotation(image, cv2.ROTATE_90_COUNTERCLOCKWISE))
+        else : 
+            cv2.imwrite(filename, image_flipping(image, 1))
+        df.loc[idx] = [file[: -4] + name, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, label]
+        idx += 1
+
+df.to_csv(path_or_buf= path + "data.csv", mode='w', index=False)

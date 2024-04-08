09c3876f2f3c7d0f787d38ab3d76f005db18bcc1
오홍석 <lkl4502@kookmin.ac.kr>
2024-04-08 23:20:01 +0900

    feat : image rotation 기능 구현

--- image_augmentation/image_augmentation.py
+++ image_augmentation/image_augmentation.py
@@ -3,3 +3,14 @@ import cv2
 def image_flipping(img, type):
     # type == 0 상하 반전, type == 1 좌우 반전
     return cv2.flip(img, type)
+
+def image_rotation(img, angle):
+    # 이미지의 크기를 잡고 이미지의 중심을 계산
+    h, w = img.shape[:2]
+    cX, cY = w // 2, h // 2
+    
+    # 이미지의 중심을 기준으로 이미지를 회전
+    M = cv2.getRotationMatrix2D((cX, cY), angle, 1.0)
+    rotated_img = cv2.warpAffine(img, M, (w, h))
+    
+    return rotated_img

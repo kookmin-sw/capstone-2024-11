2ca8285df932b2523e06d45dac8655100508e57f
오홍석 <lkl4502@kookmin.ac.kr>
2024-04-08 23:33:09 +0900

    feat : image Augmentation Test

--- image_augmentation/image_augmentation.py
+++ image_augmentation/image_augmentation.py
@@ -1,16 +1,18 @@
 import cv2
+import os
+import numpy as np
+import pandas as pd
 
 def image_flipping(img, type):
     # type == 0 상하 반전, type == 1 좌우 반전
     return cv2.flip(img, type)
 
 def image_rotation(img, angle):
-    # 이미지의 크기를 잡고 이미지의 중심을 계산
-    h, w = img.shape[:2]
-    cX, cY = w // 2, h // 2
-    
-    # 이미지의 중심을 기준으로 이미지를 회전
-    M = cv2.getRotationMatrix2D((cX, cY), angle, 1.0)
-    rotated_img = cv2.warpAffine(img, M, (w, h))
-    
-    return rotated_img
+    return cv2.rotate(img, angle)
+
+path = os.path.dirname(os.path.dirname(__file__)) + "/test/kimmingyu_rotation_270.jpeg"
+print(path)
+image = cv2.imread("./test/kimmingyu.jpeg")
+
+cv2.imwrite(path, image_rotation(image, cv2.ROTATE_90_COUNTERCLOCKWISE))
+

5e90a0fa01996c18045e9a312a37077d6054e683
오홍석 <lkl4502@kookmin.ac.kr>
2024-04-09 15:52:22 +0900

    feat : ensemble 방식인 voting과 rfc를 추가

--- PC_model/pc_model.py
+++ PC_model/pc_model.py
@@ -11,7 +11,7 @@ from Color_extract.color import extract_high_rank, save_data_csv
 
 
 from sklearn.model_selection import train_test_split
-from utils import get_evaluation, feature_plot, heatmap_plot
+from PC_model.utils import get_evaluation, feature_plot, heatmap_plot
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 
 from xgboost import XGBClassifier, plot_importance
@@ -19,6 +19,7 @@ from sklearn.multiclass import OneVsRestClassifier, OneVsOneClassifier
 from sklearn.linear_model import LinearRegression, LogisticRegression
 from sklearn.svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import VotingClassifier, RandomForestClassifier
 
 
 class PersonalColorModel:
@@ -28,6 +29,9 @@ class PersonalColorModel:
         self.ovo = OneVsOneClassifier(SVC())
         self.knn = KNeighborsClassifier(n_neighbors=5, n_jobs=-1,)
         self.lr = LogisticRegression()
+        self.voting = VotingClassifier(estimators=[("xgb", self.xgb), ("knn", self.knn),
+                                                   ("lr", self.lr)], voting='soft')
+        self.rfc = RandomForestClassifier()
 
 
     def train(self, train_x, train_y):
@@ -36,50 +40,58 @@ class PersonalColorModel:
         self.ovo.fit(train_x, train_y)
         self.knn.fit(train_x, train_y)
         self.lr.fit(train_x, train_y)
+        self.voting.fit(train_x, train_y)
+        self.rfc.fit(train_x, train_y)
 
     def test(self, test_x):
-        return self.xgb.predict(test_x), self.ovr.predict(test_x), self.ovo.predict(test_x), self.knn.predict(test_x), self.lr.predict(test_x)
+        return self.xgb.predict(test_x), self.ovr.predict(test_x), self.ovo.predict(test_x), self.knn.predict(test_x), self.lr.predict(test_x), self.voting.predict(test_x), self.rfc.predict(test_x)
+    
 
 #%%
 train_df = pd.read_csv("/Users/ohs/Desktop/capstone/personal_color_dataset/train/new_data.csv")
+test_df = pd.read_csv("../personal_color_dataset/test/new_data.csv")
 
 #%%
 # features = df.columns.drop(["filename", "label"])
 
 # print(df.columns)
 
-features = ['Hair_Red', 'Hue', 'Saturation', 'Cr', 'Cb',
-            'A', 'B', 'New Blue', 'Eye_Red', 'Eye_Blue']
+features = ['Hair_Red', 'Hue', 'Saturation', 'Cr', 'Cb', 'L',
+            'A', 'B', 'New Blue', 'Eye_Red', 'Eye_Blue', 'New Green', 'New Red']
 
 # features = ['Blue', 
 #             'Hair_Blue', 
 #             'Hue', 'Saturation', 'Value',
 #             'A', 'B', 
 #             'Eye_Blue',
-#             'New Green']
+#             'New Blue']
 
 train_x = train_df[features]
 train_y = train_df['label']
 
+test_x = test_df[features]
+y_test = test_df['label']
+
 m = PersonalColorModel()
 #%%
-mm = MinMaxScaler()
-processing_x = mm.fit_transform(train_x)
+# mm = MinMaxScaler()
+scaler = StandardScaler()
+
+scaler.fit(train_x)
 
 # %%
-test_df = pd.read_csv("../personal_color_dataset/test/new_data.csv")
-test_x = test_df[features]
-test_y = test_df['label']
+processing_train_x = scaler.transform(train_x)
+processing_test_x = scaler.transform(test_x)
 #%%
-
+# processing_test_x = mm.transform(test_x)
 # X_train, X_test, y_train, y_test = train_test_split(train_x, train_y, test_size=0.2,random_state=2024)
 
 # m.train(X_train, y_train)
 
-m.train(train_x, train_y)
+m.train(processing_train_x, train_y)
 
 # res_xgb, res_ovr, res_ovo, res_knn, res_lr = m.test(X_test)
-res_xgb, res_ovr, res_ovo, res_knn, res_lr = m.test()
+res_xgb, res_ovr, res_ovo, res_knn, res_lr, res_voting, res_rfc = m.test(processing_test_x)
 
 print("xgb 평가지표")
 get_evaluation(y_test, res_xgb)
@@ -101,8 +113,15 @@ print("lr 평가지표")
 get_evaluation(y_test, res_lr)
 print()
 
+print("voting 평가지표")
+get_evaluation(y_test, res_voting)
+print()
+
+print("rfc 평가지표")
+get_evaluation(y_test, res_rfc)
+print()
 #%%
-feature_plot(df, "label")
+feature_plot(train_df, "label")
 
 #%%
 feature_corr = train_x.corr()   

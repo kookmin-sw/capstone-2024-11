6ffc81ef073b27886b06237fed7d42b43874a6aa
오홍석 <lkl4502@kookmin.ac.kr>
2024-04-30 22:45:32 +0900

    feat : valid 이미지에 augmentation 적용을 통한 이미지 증대

--- Color_extract/color.py
+++ Color_extract/color.py
@@ -351,42 +351,40 @@ def total_data_extract(path):
     return data
 
 #%%
+valid_path = "/Users/ohs/Desktop/capstone/personal_color_dataset/valid/"
 
-# train_path = "/Users/ohs/Desktop/capstone/personal_color_dataset/train"
+for name in os.listdir(valid_path):
+    if ".csv" in name:
+        continue
+    file_name = valid_path + name[ : -4] + "_flip_horizontal.jpg"
+    img = cv2.imread(valid_path + name)
+    cv2.imwrite(file_name, cv2.flip(img, 1))
+#%%
 
-# for file_name in os.listdir(train_path):
-#     if os.path.isdir(os.path.join(train_path, file_name)):
-#         folder_path = os.path.join(train_path, file_name)
-#         size = len(os.listdir(folder_path))
+valid_df = pd.read_csv(valid_path + '/data.csv')
+size = valid_df.shape[0]
 
-#         if file_name == "spring":
-#             label = 0
-#         elif file_name == "summer":
-#             label = 1
-#         elif file_name == "fall":
-#             label = 2
-#         elif file_name == "winter":
-#             label = 3
+df = {'filename' : [0] * size, 'Red' : [0] * size, 'Green' : [0] * size, 'Blue' : [0] * size, 
+'Hue' : [0] * size, 'Saturation' : [0] * size, 'Value' : [0] * size, 
+'Y' : [0] * size, 'Cr' : [0] * size, 'Cb' : [0] * size, 
+'L' : [0] * size, 'A' : [0] * size, 'B' : [0] * size, 
+'Hair_Red' : [0] * size, 'Hair_Green' : [0] * size, 'Hair_Blue' : [0] * size,
+'Eye_Red' : [0] * size, 'Eye_Green' : [0] * size, 'Eye_Blue' : [0] * size, 
+'New Red' : [0] * size, 'New Green' : [0] * size, 'New Blue' : [0] * size, 'label' : [0] * size}
 
-#         df = {'filename' : [0] * size, 'Red' : [0] * size, 'Green' : [0] * size, 'Blue' : [0] * size, 
-#         'Hue' : [0] * size, 'Saturation' : [0] * size, 'Value' : [0] * size, 
-#         'Y' : [0] * size, 'Cr' : [0] * size, 'Cb' : [0] * size, 
-#         'L' : [0] * size, 'A' : [0] * size, 'B' : [0] * size, 
-#         'Hair_Red' : [0] * size, 'Hair_Green' : [0] * size, 'Hair_Blue' : [0] * size,
-#        'Eye_Red' : [0] * size, 'Eye_Green' : [0] * size, 'Eye_Blue' : [0] * size, 
-#        'New Red' : [0] * size, 'New Green' : [0] * size, 'New Blue' : [0] * size, "label" : [label] * size}
+columns = list(df.keys())[1 : -1]
 
-#         columns = list(df.keys())[1 : -1]
-#         print("{} 시작".format(file_name))
-#         for idx, name in enumerate(os.listdir(folder_path)):
-#             data = total_data_extract(os.path.join(folder_path, name))
-#             df['filename'][idx] = name
-#             print("진행률 : {} / {}\n".format(idx + 1, size))
-#             for col in columns:
-#                 df[col][idx] = data[col]
-        
-#         df = pd.DataFrame(df)
-#         save_data_csv(df, os.path.join(folder_path, "data.csv"))
+for idx, name in enumerate(valid_df['filename']):
+    data = total_data_extract(os.path.join(valid_path, name))
+    df['filename'][idx] = name
+    df['label'][idx] = valid_df['label'][idx]
+    print("진행률 : {} / {}\n".format(idx + 1, size))
+    for col in columns:
+        df[col][idx] = data[col]
+
+df = pd.DataFrame(df)
+print(df)
+# save_data_csv(df, os.path.join(folder_path, "data.csv"))
         
 
 # %%

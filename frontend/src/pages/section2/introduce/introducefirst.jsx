0c16059f0e4b1225b5049ceff6938cedb91a4cca
오홍석 <lkl4502@kookmin.ac.kr>
2024-05-18 23:35:58 +0900

    feat : 메인 화면 글귀 수정

--- frontend/src/pages/section2/introduce/introducefirst.jsx
+++ frontend/src/pages/section2/introduce/introducefirst.jsx
@@ -12,9 +12,13 @@ const IntroduceFirst = () => {
       <AboutText src={About}></AboutText>
       <BlackCircle>
         <Text>
-          우리 서비스는 개인 맞춤형 헤어스타일과 퍼스널 컬러를 찾는 것을 도와드려요. 전문가의 도움 없이도 손쉽게 원하는
-          스타일을 찾을 수 있어요. 컴퓨터비전과 인공지능 기술을 활용하여 빠르고 정확하게 사용자에게 어울리는 스타일을
-          추천해 드려요. 그래서 많은 비용과 시간을 들이지 않아도 만족스러운 결과를 얻을 수 있어요
+          우리 서비스는 개인 맞춤형 헤어스타일과 <br />
+          퍼스널 컬러를 찾는 것을 도와드립니다. <br />
+          전문가의 도움 없이도 손쉽게 원하는 스타일을 찾을 수 있어요. <br />
+          컴퓨터비전과 인공지능 기술을 활용하여 빠르고 정확하게 <br />
+          사용자에게 어울리는 스타일을 추천해드립니다. <br />
+          이로 인해 많은 비용과 시간을 들이지 않아도 <br />
+          만족스러운 결과를 얻을 수 있을 것이라 기대합니다.
         </Text>
       </BlackCircle>
       <Girlimg src={Girl}></Girlimg>
@@ -66,6 +70,8 @@ const BlackCircle = styled.div`
 const Text = styled.p`
   color: white;
   font-size: 1.3rem;
+  font-weight: 600;
   text-align: center;
+  line-height: 2;
 `;
 export default IntroduceFirst;
